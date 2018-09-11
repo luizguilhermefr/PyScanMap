20ccaf79c7ebcbc09713f1e50acfe4480798441c
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-09-11 02:52:06 -0300

    modify ping, tcp connections

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
+import socket
 
-import lib.ping as ping
+from ping3 import ping
 
 ARG_COUNT = 3
 MASK_LEN = 32
@@ -28,20 +29,51 @@ def make_valid_addresses(fixed_part, available_addresses):
     return address
 
 
-if len(sys.argv) != ARG_COUNT:
-    raise Exception("Invalid parameters.")
-
-address = sys.argv[1]
-mask = sys.argv[2]
-
-bin_addr = address2bin(address)
-bin_mask = address2bin(mask)
-ones_count = sum(x == '1' for x in bin_mask)
-zeros_count = MASK_LEN - ones_count
-available_addresses = 2 ** zeros_count - 2
-
-net_address = make_valid_addresses(bin_addr[:ones_count], available_addresses)
-
-for address in net_address:
+def check_ports(host, latency):
+    ports_alive = []
     for port in range(0, 2 ** 16 + 1):
-        print(address + ':' + str(port), ping.do_one(address, 1, port=port))
+        if check_port(host, port, latency):
+            ports_alive.append(port)
+    return ports_alive
+
+
+def check_port(host, port, latency):
+    s = socket.socket()
+    s.settimeout(latency * 1.5)
+    try:
+        s.connect((host, port))
+        status = True
+    except:
+        status = False
+    s.close()
+    return status
+
+
+def main():
+    if len(sys.argv) != ARG_COUNT:
+        raise Exception("Invalid parameters.")
+
+    address = sys.argv[1]
+    mask = sys.argv[2]
+
+    bin_addr = address2bin(address)
+    bin_mask = address2bin(mask)
+    ones_count = sum(x == '1' for x in bin_mask)
+    zeros_count = MASK_LEN - ones_count
+    available_addresses = 2 ** zeros_count - 2
+
+    net_address = make_valid_addresses(bin_addr[:ones_count], available_addresses)
+
+    for address in net_address:
+        latency = ping(address, ttl=1)
+        if latency is not None:
+            print('[%fs] host %s is being used..checking ports' % (latency, address))
+            ports_alive = check_ports(address, latency)
+            for port in ports_alive:
+                print('port %d is open' % port)
+        else:
+            print('host %s is not alive' % address)
+
+
+if __name__ == '__main__':
+    main()
